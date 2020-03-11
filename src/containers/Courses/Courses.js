d95706be6928666f0a2e0f120797cc6c62e234cf
Siraj Khan <sirajrkhan@live.in>
2020-03-11 17:13:17 +0530

    routing added

---
+++ src/containers/Courses/Courses.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import './Courses.css';
+
+class Courses extends Component {
+    state = {
+        courses: [
+            { id: 1, title: 'Angular - The Complete Guide' },
+            { id: 2, title: 'Vue - The Complete Guide' },
+            { id: 3, title: 'PWA - The Complete Guide' }
+        ]
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Amazing Udemy Courses</h1>
+                <section className="Courses">
+                    {
+                        this.state.courses.map( course => {
+                            return (
+                                <Link key={course.id} to={this.props.match.url+"/course"}>
+                                    <article className="Course" key={course.id}>{course.title}</article>
+                                </Link>
+                            )
+                        } )
+                    }
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Courses;
